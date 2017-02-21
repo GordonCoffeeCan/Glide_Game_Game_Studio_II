fd6ae21b361ec0e2c44f2b7e2627c1f56194e13d
GordonCoffeeCan <gordon.lee.6170@gmail.com>
2017-02-21 16:55:39 -0500

    optimized boost; add mouse click on objects

---
+++ Glide_Game/Assets/Scripts/MouseClickManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector2 _rayPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D _rayHit = Physics2D.Raycast(new Vector2(_rayPosition.x, _rayPosition.y), Camera.main.transform.forward);
+            Debug.Log(_rayHit.collider.name);
+        }
+	}
+}
--- Glide_Game/Assets/Scripts/playerControlScript.cs
+++ Glide_Game/Assets/Scripts/playerControlScript.cs
@@ -7,14 +7,13 @@ public class playerControlScript : MonoBehaviour
 
 
     public float speed = 5;
+    public float boostSpeed = 35;
     public float jumpAmount;
     public float glideAmount;
-    public float diveAmount;
+    public float diveAmount = 10;
 
     
     public KeyCode downKey = KeyCode.S;
-    //public KeyCode leftKey = KeyCode.A;
-    //public KeyCode rightKey = KeyCode.D;
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode diveKey = KeyCode.LeftShift;
 
@@ -33,102 +32,81 @@ public class playerControlScript : MonoBehaviour
     public GameObject wing1;
     public GameObject wing2;
 
-    private float lastVelocityX;
-    private bool isBoost = false;
-
     // Use this for initialization
     void Start()
     {
-
         ShowWings(wing1, wing2, false);
-
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+        Glide();
+        Dive();
+        Boost();
 
+        rb.gravityScale = playerGravityScale;
+        
+    }
+
+    private void FixedUpdate() {
+        Move(Vector3.right, "Horizontal");
+    }
+
+    void Move(Vector3 _dir, string _axis)
+    {
+        rb.velocity = new Vector2(_dir.x * Input.GetAxis(_axis) * speed, rb.velocity.y);
+        
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(jumpKey) && onGround == true){
+            rb.AddForce(Vector3.up * jumpAmount);
+            onGround = false;
+
+        }else if (Input.GetKeyDown(jumpKey))
+        {
+            rb.gravityScale = glideAmount;
+            ShowWings(wing1, wing2, true);
+            isGliding = true;
+      
+        }else if (Input.GetKeyUp(jumpKey)){
+            rb.gravityScale = 1;
+            ShowWings(wing1, wing2, false);
+            isGliding = false;
+        }
+    }
+
+    private void Glide() {
         if (isGliding == true) {
             playerGravityScale = glideAmount;
         } else {
             playerGravityScale = 1;
             windPush._windPushed = false;
         }
-        rb.gravityScale = playerGravityScale;
+    }
 
-        if (Input.GetKey(KeyCode.LeftAlt)) {
-            isBoost = true;
-        }else if (Input.GetKeyUp(KeyCode.LeftAlt)) {
-            isBoost = false;
-        }
-
-        if (rb.velocity.x > 0) {
-            lastVelocityX = 1;
-        } else if (rb.velocity.x < 0) {
-            lastVelocityX = -1;
-        } else {
-            lastVelocityX = 0;
+    private void Dive() {
+        if (Input.GetKey(diveKey)) {
+            playerGravityScale = diveAmount;
+            ShowWings(wing1, wing2, false);
+            isGliding = false;
+        } else if (Input.GetKeyUp(diveKey)) {
+            playerGravityScale = 1;
         }
     }
 
-    private void FixedUpdate() {
-        //Move(Vector3.down, downKey);
-        if (isBoost == false) {
-            Move(Vector3.right, "Horizontal");
-        } else {
-            Boost();
-        }
-    }
-
-    void Move(Vector3 _dir, string _axis)
-    {
-        rb.velocity = new Vector2(_dir.x * Input.GetAxis(_axis) * speed, rb.velocity.y);
-    }
-
-    void Jump()
-    {
-        
-
-        if (Input.GetKeyDown(jumpKey) && onGround == true)
-        {
-            rb.AddForce(Vector3.up * jumpAmount);
-            onGround = false;
-
-        }
-        else if (Input.GetKeyDown(jumpKey))
-        {
-            rb.gravityScale = glideAmount;
-            ShowWings(wing1, wing2, true);
-            isGliding = true;
-      
+    private void Boost() {
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) {
+            speed = boostSpeed;
         }
 
-        else if (Input.GetKeyDown(diveKey))
-        {
-            rb.gravityScale = diveAmount;
-            ShowWings(wing1, wing2, false);
-            isGliding = false;
+        speed -= 0.8f;
+        if (speed <= 5) {
+            speed = 5;
         }
-
-        else if (Input.GetKeyUp(jumpKey))
-        {
-            rb.gravityScale = 1;
-            ShowWings(wing1, wing2, false);
-            isGliding = false;
-        }
-
-
-        else if (Input.GetKeyUp(diveKey))
-        {
-            rb.gravityScale = 5;
-            ShowWings(wing1, wing2, false);
-            isGliding = false;
-        }
-
-
-
-
     }
 
     void OnCollisionEnter2D(Collision2D collider)
@@ -137,15 +115,10 @@ public class playerControlScript : MonoBehaviour
         {
             onGround = true;
         }
-        //Debug.Log("Dragon");
     }
 
     private void ShowWings(GameObject _wing1, GameObject _wing2, bool _isShow = false) {
         _wing1.SetActive(_isShow);
         _wing2.SetActive(_isShow);
-    }
-
-    private void Boost() {
-        rb.velocity = new Vector2(lastVelocityX * speed * 3, rb.velocity.y);
     }
 }
